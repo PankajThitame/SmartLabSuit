a3cd30ccb48106112689fe730137ca5f022f65d3
PankajThitame <thitamepankaj@gmail.com>
2025-05-13 16:31:25 +0530

    new react

---
+++ SmartLabSuit-Frontend/SmartLabSuit-Backend/src/main/java/com/nivpune/SmartLabSuit/SmartLabSuitApplication.java
@@ -0,0 +1,13 @@
+package com.nivpune.SmartLabSuit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLabSuitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLabSuitApplication.class, args);
+	}
+
+}
---
+++ SmartLabSuit-Frontend/SmartLabSuit-Backend/src/test/java/com/nivpune/SmartLabSuit/SmartLabSuitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nivpune.SmartLabSuit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLabSuitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
